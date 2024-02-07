31d0110fee9a62f47cc8cb3b5c46685171d08eeb
Jorge Henrique <jorgehasd@gmail.com>
2024-02-07 19:38:00 -0300

    feat: add extrato api

--- rinha-app/Program.cs
+++ rinha-app/Program.cs
@@ -5,13 +5,43 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.MapPost("/clientes/{id}/transacoes", async (int id, Transacao transacao) => {
+
     Console.WriteLine("Id: " + id);
     Console.WriteLine("Transacao: " + transacao);
+
+    return Results.Ok(new TransacaoExecutada(100000, -9098));
+});
+
+app.MapGet("/clientes/{id}/extrato", async (int id) => {
+
+    Console.WriteLine("Id: " + id);
+
+    return Results.Ok(new Extrato(
+        new Saldo(-9098, "2024-01-17T02:34:41.217753Z", 100000),
+        new List<Transacao>{
+            new Transacao(10, "c", "descricao", "2024-01-17T02:34:38.543030Z"),
+            new Transacao(90000, "d", "descricao", "2024-01-17T02:34:38.543030Z")
+        }
+    ));
 });
 
 app.Run();
 
-record Transacao(int valor, string tipo, string descricao)
+record Transacao(int valor, string tipo, string descricao, string realizada_em)
 {
+
+}
+
+record TransacaoExecutada(int limite, int saldo)
+{
+
+}
+
+record Extrato(Saldo saldo, List<Transacao> ultimas_transacoes)
+{
+
+}
+
+record Saldo(int total, string data_extrato, int limite){
 
 }
