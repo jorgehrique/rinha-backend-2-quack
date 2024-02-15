bef95b72a63b7dc8730c965ef35410f830e66806
Jorge Henrique <jorgehasd@gmail.com>
2024-02-15 07:32:06 -0300

    feat: change to aot minimal api

--- rinha-app/Program.cs
+++ rinha-app/Program.cs
@@ -1,57 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+var sampleTodos = new Todo[] {
+    new(1, "Walk the dog"),
+    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
+    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
+    new(4, "Clean the bathroom"),
+    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+};
 
-app.MapPost("/clientes/{id}/transacoes", (int id, Transacao transacao) => {
-
-    Console.WriteLine("Id: " + id);
-    Console.WriteLine("Transacao: " + transacao);
-
-    if(!isTransacaoValid(transacao)){
-        return Results.BadRequest();
-    }
-
-    return Results.Ok(new TransacaoExecutada(100000, -9098));
-});
-
-app.MapGet("/clientes/{id}/extrato", (int id) => {
-
-    Console.WriteLine("Id: " + id);
-
-    return Results.Ok(new Extrato(
-        new Saldo(-9098, "2024-01-17T02:34:41.217753Z", 100000),
-        new List<Transacao>{
-            new Transacao(10, 'c', "descricao", "2024-01-17T02:34:38.543030Z"),
-            new Transacao(90000, 'd', "descricao", "2024-01-17T02:34:38.543030Z")
-        }
-    ));
-});
+var todosApi = app.MapGroup("/todos");
+todosApi.MapGet("/", () => sampleTodos);
+todosApi.MapGet("/{id}", (int id) =>
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());
 
 app.Run();
 
-bool isTransacaoValid(Transacao transacao){
-    return transacao.valor > 0
-        && transacao.descricao.Length >= 1 && transacao.descricao.Length <= 10
-        && (transacao.tipo != 'c' ||  transacao.tipo != 'd');
-}
+public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
 
-record Transacao(int valor, char tipo, string descricao, string? realizada_em)
+[JsonSerializable(typeof(Todo[]))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
 {
-
-}
-
-record TransacaoExecutada(int limite, int saldo)
-{
-
-}
-
-record Extrato(Saldo saldo, List<Transacao> ultimas_transacoes)
-{
-
-}
-
-record Saldo(int total, string data_extrato, int limite){
 
 }
